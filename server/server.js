7137f7d96582e7c9a485ddac56b6b92953e73ebd
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-01-14 10:46:14 +0630

    New Updated

--- server/server.js
+++ server/server.js
@@ -38,6 +38,7 @@ app.post("/", async (req, res) => {
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
+    console.log(response);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
